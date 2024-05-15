73a9960c048f758cab7a5c1b2609ac92c152a7ab
chundrac <chundra.a.cathcart@gmail.com>
2024-05-15 13:17:27 +0200

    comparison

--- regression_models/model_comparison.R
+++ regression_models/model_comparison.R
@@ -2,7 +2,9 @@ require(brms)
 
 loglik.pred.dist <- NULL
 
-for (i in 1:10) {
+dom.inds <- c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 43, 44, 45, 46, 47, 48, 49, 50)
+
+for (i in dom.inds) {
   load(paste('output/model_fit_dominant_frequency_pred.dist_',i,'.Rdata',sep=''))
   loglik <- log_lik(model)
   loglik.pred.dist <- rbind(loglik.pred.dist,loglik)
@@ -10,7 +12,7 @@ for (i in 1:10) {
 
 loglik.pred.nodist <- NULL
 
-for (i in 1:10) {
+for (i in dom.inds) {
   load(paste('output/model_fit_dominant_frequency_pred.nodist_',i,'.Rdata',sep=''))
   loglik <- log_lik(model)
   loglik.pred.nodist <- rbind(loglik.pred.nodist,loglik)
@@ -18,7 +20,7 @@ for (i in 1:10) {
 
 loglik.nopred.dist <- NULL
 
-for (i in 1:10) {
+for (i in dom.inds) {
   load(paste('output/model_fit_dominant_frequency_nopred.dist_',i,'.Rdata',sep=''))
   loglik <- log_lik(model)
   loglik.nopred.dist <- rbind(loglik.nopred.dist,loglik)
@@ -26,7 +28,7 @@ for (i in 1:10) {
 
 loglik.nopred.nodist <- NULL
 
-for (i in 1:10) {
+for (i in dom.inds) {
   load(paste('output/model_fit_dominant_frequency_nopred.nodist_',i,'.Rdata',sep=''))
   loglik <- log_lik(model)
   loglik.nopred.nodist <- rbind(loglik.nopred.nodist,loglik)
@@ -36,9 +38,11 @@ print("dominant frequency")
 dominant.compare <- loo_compare(list(loo(loglik.pred.dist),loo(loglik.pred.nodist),loo(loglik.nopred.dist),loo(loglik.nopred.nodist)))
 dominant.stack <- loo_model_weights(list(loo(loglik.pred.dist),loo(loglik.pred.nodist),loo(loglik.nopred.dist),loo(loglik.nopred.nodist)))
 
+inds <- c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 43, 44, 45, 46, 47, 48, 49, 50)
+
 loglik.pred.dist <- NULL
 
-for (i in 1:10) {
+for (i in inds) {
   load(paste('output/model_fit_frequency_pred.dist_',i,'.Rdata',sep=''))
   loglik <- log_lik(model)
   loglik.pred.dist <- rbind(loglik.pred.dist,loglik)
@@ -46,7 +50,7 @@ for (i in 1:10) {
 
 loglik.pred.nodist <- NULL
 
-for (i in 1:10) {
+for (i in inds) {
   load(paste('output/model_fit_frequency_pred.nodist_',i,'.Rdata',sep=''))
   loglik <- log_lik(model)
   loglik.pred.nodist <- rbind(loglik.pred.nodist,loglik)
@@ -54,7 +58,7 @@ for (i in 1:10) {
 
 loglik.nopred.dist <- NULL
 
-for (i in 1:10) {
+for (i in inds) {
   load(paste('output/model_fit_frequency_nopred.dist_',i,'.Rdata',sep=''))
   loglik <- log_lik(model)
   loglik.nopred.dist <- rbind(loglik.nopred.dist,loglik)
@@ -62,7 +66,7 @@ for (i in 1:10) {
 
 loglik.nopred.nodist <- NULL
 
-for (i in 1:10) {
+for (i in inds) {
   load(paste('output/model_fit_frequency_nopred.nodist_',i,'.Rdata',sep=''))
   loglik <- log_lik(model)
   loglik.nopred.nodist <- rbind(loglik.nopred.nodist,loglik)
