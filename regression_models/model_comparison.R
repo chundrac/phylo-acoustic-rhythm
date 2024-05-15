190dc7bfb50126f3ad4f3429996bca68735808f2
chundrac <chundra.a.cathcart@gmail.com>
2024-05-15 13:38:38 +0200

    fix

--- regression_models/model_comparison.R
+++ regression_models/model_comparison.R
@@ -38,7 +38,8 @@ print("dominant frequency")
 dominant.compare <- loo_compare(list(loo(loglik.pred.dist),loo(loglik.pred.nodist),loo(loglik.nopred.dist),loo(loglik.nopred.nodist)))
 dominant.stack <- loo_model_weights(list(loo(loglik.pred.dist),loo(loglik.pred.nodist),loo(loglik.nopred.dist),loo(loglik.nopred.nodist)))
 
-inds <- c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 43, 44, 45, 46, 47, 48, 49, 50)
+#inds <- c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 43, 44, 45, 46, 47, 48, 49, 50)
+inds <- c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 42, 43, 44, 45, 47, 48, 49, 50)
 
 loglik.pred.dist <- NULL
 
