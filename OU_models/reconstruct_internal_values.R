3dbed8db8b88f740721c81710ef83f7eeed7f460
Chundra Cathcart <chundra.a.cathcart@gmail.com>
2024-08-29 22:39:55 +0200

    graphics

--- OU_models/reconstruct_internal_values.R
+++ OU_models/reconstruct_internal_values.R
@@ -295,3 +295,31 @@ ggtree(mcc.tree,alpha=.3) %<+% medians.taxon + xlim(-.1, 1) +
   scale_color_gradient2(low = brewer.pal(9, "YlGn")[1], midpoint = 6.5, mid = brewer.pal(9, "YlGn")[5], high = brewer.pal(9, "YlGn")[9], name = "Median rhythm value") + 
   theme(legend.position = c(.25,.85))
 dev.off()
+
+pdf('reconstruction6.pdf',width=7.5,height=7.5)
+ggtree(mcc.tree,alpha=.3) %<+% medians.taxon + xlim(-.1, 1) + 
+  geom_tiplab(offset = .01, hjust = 0, size=2) + 
+  geom_point(aes(color = medians.node, size = medians.node), alpha=.5) + 
+  scale_size_continuous(name='Median rhythm value') + 
+  scale_color_gradient2(low = brewer.pal(9, "YlGn")[1], midpoint = 6.5, mid = brewer.pal(9, "YlGn")[5], high = brewer.pal(9, "YlGn")[9], name = "Median rhythm value") + 
+  theme(legend.position = c(.25,.725))
+dev.off()
+
+pdf('reconstruction7.pdf',width=7.5,height=7.5)
+ggtree(mcc.tree,alpha=.3) %<+% medians.taxon + xlim(-.1, 1) + 
+  geom_tiplab(offset = .01, hjust = 0, size=2) + 
+  geom_point(aes(color = medians.node, size = medians.node), alpha=.5) + 
+  scale_size_continuous(name='Median rhythm value') + 
+  scale_color_gradient2(low = brewer.pal(9, "YlGn")[1], midpoint = 6.5, mid = brewer.pal(9, "YlGn")[5], high = brewer.pal(9, "YlGn")[9], name = "Median rhythm value") + 
+  theme(legend.position = c(.25,.725)) + 
+  geom_text(aes(label=as.character(round(medians.node,1))),size=1.5)
+dev.off()
+
+pdf('reconstruction8.pdf',width=7.5,height=7.5)
+ggtree(mcc.tree,alpha=.3) %<+% medians.taxon + xlim(-.1, 1) + 
+  geom_tiplab(offset = .01, hjust = 0, size=2) + 
+  geom_point(aes(color = medians.node), alpha=.5) + 
+  scale_color_gradient2(low = brewer.pal(9, "YlGn")[1], midpoint = 6.5, mid = brewer.pal(9, "YlGn")[5], high = brewer.pal(9, "YlGn")[9], name = "Median rhythm value") + 
+  theme(legend.position = c(.25,.85)) + 
+  geom_text(aes(label=as.character(round(medians.node,1))),size=1.5)
+dev.off()
