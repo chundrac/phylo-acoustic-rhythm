976b92d966e299a38a88d262b91cab55b31b368d
Chundra Cathcart <chundra.a.cathcart@gmail.com>
2025-10-14 11:39:09 +0200

    birds and mammals

---
+++ OU_models/run_models_birdsandmammals.sh
@@ -0,0 +1,7 @@
+for i in {1..50}
+do
+    for modtype in 'BM_UV_dist' 'BM_UV_nondist' 'OU_UV_dist' 'OU_UV_nondist'
+    do
+	sbatch run_job.sh Rscript run_model_birdsandmammals.R $modtype $i
+    done
+done
