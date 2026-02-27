8d2808073f7213719a2ba88b2420f19692ec2b3f
chundrac <chundra.a.cathcart@gmail.com>
2026-02-27 11:53:14 +0100

    deleted

--- regression_models/run_all_models.sh
+++
@@ -1,9 +0,0 @@
-for i in {1..10}
-do
-    for modtype in pred.dist pred.nodist nopred.dist nopred.nodist
-    do
-	sbatch run_job.sh Rscript run_model_frequency.R $modtype $i
-	sbatch run_job.sh Rscript run_model_dominant_frequency.R $modtype $i
-    done
-done
-
